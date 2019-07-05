98d99035ad7038945a071dc5ba6740720a2272b5
Fahien <sir.fahien@gmail.com>
2019-07-05 22:29:43 +0100

    Fix Mat4 const subscript and format

--- include/mathspot/Math.h
+++ include/mathspot/Math.h
@@ -8,37 +8,35 @@
 
 namespace mathspot
 {
-
-
 const float kPi{ 3.14159265f };
 
 /// Degrees to radians conversion
 /// @param[in] degrees Angle in degrees
 /// @return Angle in radians
-float radians(const float degrees);
+float radians( const float degrees );
 
 /// Radians to degrees conversion
 /// @param[in] radians Angle in radians
 /// @return Angle in degrees
-float degrees(const float radians);
+float degrees( const float radians );
 
 
 class Size
 {
-public:
+  public:
 	static Size null;
 
 	Size();
-	Size(int w, int h);
+	Size( int w, int h );
 
-	Size&      operator*=(const int&   f);
-	Size&      operator*=(const float& f);
-	Size&      operator/=(const int&   i);
-	const Size operator/ (const int&   i) const;
+	Size&      operator*=( const int& f );
+	Size&      operator*=( const float& f );
+	Size&      operator/=( const int& i );
+	const Size operator/( const int& i ) const;
 
-	const bool operator==(const Size& other) const;
+	const bool operator==( const Size& other ) const;
 
-	friend std::ostream& operator<<(std::ostream& os, const Size& v);
+	friend std::ostream& operator<<( std::ostream& os, const Size& v );
 
 	int width;
 	int height;
@@ -47,21 +45,21 @@ public:
 
 class PYSPOT_EXPORT Vec2
 {
-public:
+  public:
 	static Vec2 zero;
 
 	Vec2();
-	Vec2(const float xx, const float yy);
+	Vec2( const float xx, const float yy );
 
 	void Normalize();
 
-	Vec2&      operator= (const Vec2& other);
-	Vec2&      operator+=(const Vec2& other);
-	const Vec2 operator+ (const Vec2& other) const;
+	Vec2&      operator=( const Vec2& other );
+	Vec2&      operator+=( const Vec2& other );
+	const Vec2 operator+( const Vec2& other ) const;
 
-	const bool operator==(const Vec2& other) const;
+	const bool operator==( const Vec2& other ) const;
 
-	friend std::ostream& operator<<(std::ostream& os, const Vec2& v);
+	friend std::ostream& operator<<( std::ostream& os, const Vec2& v );
 
 	float x;
 	float y;
@@ -70,37 +68,37 @@ public:
 
 class PYSPOT_EXPORT Vec3
 {
-public:
+  public:
 	static Vec3 zero;
 
-	Vec3(const float xx = 0.0f, const float yy = 0.0f, const float zz = 0.0f);
-	Vec3(const Vec3& other);
-	Vec3(Vec3&& other);
+	Vec3( const float xx = 0.0f, const float yy = 0.0f, const float zz = 0.0f );
+	Vec3( const Vec3& other );
+	Vec3( Vec3&& other );
 
-	static Vec3 Cross(const Vec3& a, const Vec3& b);
+	static Vec3 Cross( const Vec3& a, const Vec3& b );
 
-	void Set(const float xx, const float yy, const float zz);
-	void Normalize();
-	Vec3& operator= (const Vec3& other);
-	Vec3& operator+=(const Vec3& other);
+	void  Set( const float xx, const float yy, const float zz );
+	void  Normalize();
+	Vec3& operator=( const Vec3& other );
+	Vec3& operator+=( const Vec3& other );
 	Vec3  operator-() const;
 
-	const bool operator==(const Vec3& other) const;
+	const bool operator==( const Vec3& other ) const;
 
 	float x;
 	float y;
 	float z;
 };
 
-std::ostream& operator<<(std::ostream& os, const Vec3& v);
+std::ostream& operator<<( std::ostream& os, const Vec3& v );
 
 
 class Quat
 {
-public:
+  public:
 	static Quat identity;
 
-	Quat(float x = 0.0f, float y = 0.0f, float z = 0.0f, float w = 0.0f);
+	Quat( float x = 0.0f, float y = 0.0f, float z = 0.0f, float w = 0.0f );
 
 	float x;
 	float y;
@@ -111,39 +109,39 @@ public:
 
 class Mat4
 {
-public:
+  public:
 	static Mat4 zero;
 	static Mat4 identity;
 
 	Mat4();
-	Mat4(std::initializer_list<float>);
-	Mat4(const float* const m);
-	Mat4(const Quat& quat);
+	Mat4( std::initializer_list<float> );
+	Mat4( const float* const m );
+	Mat4( const Quat& quat );
 
-	float&     operator[](const int index);
-	float      operator[](const int index) const;
-	Mat4&      operator= (const Mat4& matrix);
-	Mat4&      operator+=(const Mat4& matrix);
-	const Mat4 operator+ (const Mat4& other) const;
-	Mat4&      operator*=(const Mat4& matrix);
-	const Mat4 operator* (const Mat4& other) const;
+	float&       operator[]( const int index );
+	const float& operator[]( const int index ) const;
+	Mat4&        operator=( const Mat4& matrix );
+	Mat4&        operator+=( const Mat4& matrix );
+	const Mat4   operator+( const Mat4& other ) const;
+	Mat4&        operator*=( const Mat4& matrix );
+	const Mat4   operator*( const Mat4& other ) const;
 
-	const bool operator==(const Mat4& other) const;
+	const bool operator==( const Mat4& other ) const;
 
-	void Translate(const Vec3& vec);
-	void TranslateX(const float amount);
-	void TranslateY(const float amount);
-	void TranslateZ(const float amount);
+	void Translate( const Vec3& vec );
+	void TranslateX( const float amount );
+	void TranslateY( const float amount );
+	void TranslateZ( const float amount );
 
-	void Scale(const Vec3& scale);
-	void ScaleX(const float scale);
-	void ScaleY(const float scale);
-	void ScaleZ(const float scale);
+	void Scale( const Vec3& scale );
+	void ScaleX( const float scale );
+	void ScaleY( const float scale );
+	void ScaleZ( const float scale );
 
-	void Rotate(const Quat& quat);
-	void RotateX(const float radians);
-	void RotateY(const float radians);
-	void RotateZ(const float radians);
+	void Rotate( const Quat& quat );
+	void RotateX( const float radians );
+	void RotateY( const float radians );
+	void RotateZ( const float radians );
 
 	float matrix[16];
 };
@@ -151,20 +149,20 @@ public:
 
 class Rectangle
 {
-public:
+  public:
 	static Rectangle zero;
 
 	Rectangle();
 
-	const bool operator==(const Rectangle& other) const;
+	const bool operator==( const Rectangle& other ) const;
 
 	/// Tests whether (x, y) is inside the rectangle
-	bool Contains(float x, float y);
+	bool Contains( float x, float y );
 
 	/// Tests whether this rectangle intersects another one
-	bool Intersects(const Rectangle& other);
+	bool Intersects( const Rectangle& other );
 	/// Tests whether this rectangle intersects another one
-	bool Intersects(const Rectangle* other);
+	bool Intersects( const Rectangle* other );
 
 	float x;
 	float y;
@@ -173,7 +171,7 @@ public:
 };
 
 
-}
+}  // namespace mathspot
 
 
-#endif // MST_MATH_H_
+#endif  // MST_MATH_H_
--- src/Math.cpp
+++ src/Math.cpp
@@ -6,13 +6,13 @@
 using namespace mathspot;
 
 
-float mathspot::radians(const float degrees)
+float mathspot::radians( const float degrees )
 {
 	return degrees * kPi / 180.0f;
 }
 
 
-float mathspot::degrees(const float radians)
+float mathspot::degrees( const float radians )
 {
 	return radians * 180.0f / kPi;
 }
@@ -22,55 +22,57 @@ Size Size::null{};
 
 
 Size::Size()
-:	width { 0 }
-,	height{ 0 }
-{}
-
-
-Size::Size(int w, int h)
-:	width { w }
-,	height{ h }
-{}
-
-
-Size& Size::operator*=(const int& f)
+    : width{ 0 }
+    , height{ 0 }
 {
-	width  *= f;
+}
+
+
+Size::Size( int w, int h )
+    : width{ w }
+    , height{ h }
+{
+}
+
+
+Size& Size::operator*=( const int& f )
+{
+	width *= f;
 	height *= f;
 	return *this;
 }
 
 
-Size& Size::operator*=(const float& f)
+Size& Size::operator*=( const float& f )
 {
-	width  = static_cast<int>(width  * f);
-	height = static_cast<int>(height * f);
+	width  = static_cast<int>( width * f );
+	height = static_cast<int>( height * f );
 	return *this;
 }
 
 
-Size& Size::operator/=(const int& i)
+Size& Size::operator/=( const int& i )
 {
-	width  /= i;
+	width /= i;
 	height /= i;
 	return *this;
 }
 
 
-const Size Size::operator/(const int& i) const
+const Size Size::operator/( const int& i ) const
 {
 	Size result = *this;
 	return result /= i;
 }
 
 
-const bool Size::operator==(const Size& other) const
+const bool Size::operator==( const Size& other ) const
 {
 	return width == other.width && height == other.height;
 }
 
 
-std::ostream& operator<<(std::ostream& os, const Size& s)
+std::ostream& operator<<( std::ostream& os, const Size& s )
 {
 	return os << "[" << s.width << ", " << s.height << "]";
 }
@@ -80,26 +82,28 @@ Vec2 Vec2::zero{};
 
 
 Vec2::Vec2()
-:	x{ 0.0f }
-,	y{ 0.0f }
-{}
+    : x{ 0.0f }
+    , y{ 0.0f }
+{
+}
 
 
-Vec2::Vec2(const float xx, const float yy)
-:	x{ xx }
-,	y{ yy }
-{}
+Vec2::Vec2( const float xx, const float yy )
+    : x{ xx }
+    , y{ yy }
+{
+}
 
 
 void Vec2::Normalize()
 {
-	float length{ sqrtf(x * x + y * y) };
+	float length{ sqrtf( x * x + y * y ) };
 	x /= length;
 	y /= length;
 }
 
 
-Vec2& Vec2::operator=(const Vec2& other)
+Vec2& Vec2::operator=( const Vec2& other )
 {
 	x = other.x;
 	y = other.y;
@@ -107,7 +111,7 @@ Vec2& Vec2::operator=(const Vec2& other)
 }
 
 
-Vec2& Vec2::operator+=(const Vec2& other)
+Vec2& Vec2::operator+=( const Vec2& other )
 {
 	x += other.x;
 	y += other.y;
@@ -115,20 +119,20 @@ Vec2& Vec2::operator+=(const Vec2& other)
 }
 
 
-const Vec2 Vec2::operator+(const Vec2& other) const
+const Vec2 Vec2::operator+( const Vec2& other ) const
 {
 	Vec2 result = *this;
 	return result += other;
 }
 
 
-const bool Vec2::operator==(const Vec2& other) const
+const bool Vec2::operator==( const Vec2& other ) const
 {
 	return x == other.x && y == other.y;
 }
 
 
-std::ostream& operator<<(std::ostream& os, const Vec2& v)
+std::ostream& operator<<( std::ostream& os, const Vec2& v )
 {
 	return os << "[" << v.x << ", " << v.y << "]";
 }
@@ -137,28 +141,31 @@ std::ostream& operator<<(std::ostream& os, const Vec2& v)
 Vec3 Vec3::zero{};
 
 
-Vec3::Vec3(const float xx, const float yy, const float zz)
-:	x{ xx }
-,	y{ yy }
-,	z{ zz }
-{}
+Vec3::Vec3( const float xx, const float yy, const float zz )
+    : x{ xx }
+    , y{ yy }
+    , z{ zz }
+{
+}
 
 
-Vec3::Vec3(const Vec3& other)
-:	x{ other.x }
-,	y{ other.y }
-,	z{ other.z }
-{}
+Vec3::Vec3( const Vec3& other )
+    : x{ other.x }
+    , y{ other.y }
+    , z{ other.z }
+{
+}
 
 
-Vec3::Vec3(Vec3&& other)
-:	x{ other.x }
-,	y{ other.y }
-,	z{ other.z }
-{}
+Vec3::Vec3( Vec3&& other )
+    : x{ other.x }
+    , y{ other.y }
+    , z{ other.z }
+{
+}
 
 
-Vec3 Vec3::Cross(const Vec3& a, const Vec3& b)
+Vec3 Vec3::Cross( const Vec3& a, const Vec3& b )
 {
 	Vec3 result{};
 	result.x = a.y * b.z - a.z * b.y;
@@ -168,7 +175,7 @@ Vec3 Vec3::Cross(const Vec3& a, const Vec3& b)
 }
 
 
-void Vec3::Set(const float xx, const float yy, const float zz)
+void Vec3::Set( const float xx, const float yy, const float zz )
 {
 	x = xx;
 	y = yy;
@@ -178,14 +185,14 @@ void Vec3::Set(const float xx, const float yy, const float zz)
 
 void Vec3::Normalize()
 {
-	float length{ sqrtf(x * x + y * y + z * z) };
+	float length{ sqrtf( x * x + y * y + z * z ) };
 	x /= length;
 	y /= length;
 	z /= length;
 }
 
 
-Vec3& Vec3::operator=(const Vec3& other)
+Vec3& Vec3::operator=( const Vec3& other )
 {
 	x = other.x;
 	y = other.y;
@@ -194,7 +201,7 @@ Vec3& Vec3::operator=(const Vec3& other)
 }
 
 
-Vec3& Vec3::operator+=(const Vec3& other)
+Vec3& Vec3::operator+=( const Vec3& other )
 {
 	x += other.x;
 	y += other.y;
@@ -209,13 +216,13 @@ Vec3 Vec3::operator-() const
 }
 
 
-const bool Vec3::operator==(const Vec3& other) const
+const bool Vec3::operator==( const Vec3& other ) const
 {
 	return x == other.x && y == other.y && z == other.z;
 }
 
 
-std::ostream& operator<<(std::ostream& os, const Vec3& v)
+std::ostream& operator<<( std::ostream& os, const Vec3& v )
 {
 	return os << "[" << v.x << ", " << v.y << ", " << v.z << "]";
 }
@@ -224,38 +231,34 @@ std::ostream& operator<<(std::ostream& os, const Vec3& v)
 Quat Quat::identity{ 0.0f, 0.0f, 0.0f, 1.0f };
 
 
-Quat::Quat(float xx, float yy, float zz, float ww)
-:	x{ xx }
-,	y{ yy }
-,	z{ zz }
-,	w{ ww }
-{}
+Quat::Quat( float xx, float yy, float zz, float ww )
+    : x{ xx }
+    , y{ yy }
+    , z{ zz }
+    , w{ ww }
+{
+}
 
 
 Mat4 Mat4::zero{};
 
 
-Mat4 Mat4::identity
-{
-	1, 0, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 1, 0,
-	0, 0, 0, 1
-};
+Mat4 Mat4::identity{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 
 
 Mat4::Mat4()
-:	matrix{ 0.0f }
-{}
+    : matrix{ 0.0f }
+{
+}
 
 
-Mat4::Mat4(std::initializer_list<float> list)
+Mat4::Mat4( std::initializer_list<float> list )
 {
 	int i{ 0 };
-	for (float value : list)
+	for ( float value : list )
 	{
 		matrix[i++] = value;
-		if (i == 16)
+		if ( i == 16 )
 		{
 			break;
 		}
@@ -263,22 +266,18 @@ Mat4::Mat4(std::initializer_list<float> list)
 }
 
 
-Mat4::Mat4(const float* const m)
+Mat4::Mat4( const float* const m )
 {
-	for (int i{ 0 }; i < 16; ++i)
+	for ( int i{ 0 }; i < 16; ++i )
 	{
 		matrix[i] = m[i];
 	}
 }
 
 
-Mat4::Mat4(const Quat& q)
+Mat4::Mat4( const Quat& q )
 {
-	float s{ 2.0f /
-	         (q.x * q.x +
-	          q.y * q.y +
-	          q.z * q.z +
-	          q.w * q.w) };
+	float s{ 2.0f / ( q.x * q.x + q.y * q.y + q.z * q.z + q.w * q.w ) };
 
 	float xs{ s * q.x };
 	float ys{ s * q.y };
@@ -296,19 +295,19 @@ Mat4::Mat4(const Quat& q)
 	float yz{ q.y * zs };
 	float zz{ q.z * zs };
 
-	matrix[0]  = 1.0f - (yy + zz);
+	matrix[0]  = 1.0f - ( yy + zz );
 	matrix[4]  = xy - wz;
 	matrix[8]  = xz + wy;
 	matrix[12] = 0.0f;
 
 	matrix[1]  = xy + wz;
-	matrix[5]  = 1.0f - (xx + zz);
+	matrix[5]  = 1.0f - ( xx + zz );
 	matrix[9]  = yz - wx;
 	matrix[13] = 0.0f;
 
 	matrix[2]  = xz - wy;
 	matrix[6]  = yz + wx;
-	matrix[10] = 1.0f - (xx + yy);
+	matrix[10] = 1.0f - ( xx + yy );
 	matrix[14] = 0.0f;
 
 	matrix[3]  = 0.0f;
@@ -318,21 +317,21 @@ Mat4::Mat4(const Quat& q)
 }
 
 
-float& Mat4::operator[](const int index)
+float& Mat4::operator[]( const int index )
 {
 	return matrix[index];
 }
 
 
-float Mat4::operator[](const int index) const
+const float& Mat4::operator[]( const int index ) const
 {
 	return matrix[index];
 }
 
 
-Mat4& Mat4::operator=(const Mat4& other)
+Mat4& Mat4::operator=( const Mat4& other )
 {
-	for (int i{ 0 }; i < 16; ++i)
+	for ( int i{ 0 }; i < 16; ++i )
 	{
 		matrix[i] = other.matrix[i];
 	}
@@ -340,9 +339,9 @@ Mat4& Mat4::operator=(const Mat4& other)
 }
 
 
-Mat4& Mat4::operator+=(const Mat4& other)
+Mat4& Mat4::operator+=( const Mat4& other )
 {
-	for (int i{ 0 }; i < 16; ++i)
+	for ( int i{ 0 }; i < 16; ++i )
 	{
 		matrix[i] += other.matrix[i];
 	}
@@ -350,27 +349,25 @@ Mat4& Mat4::operator+=(const Mat4& other)
 }
 
 
-const Mat4 Mat4::operator+(const Mat4& other) const
+const Mat4 Mat4::operator+( const Mat4& other ) const
 {
 	Mat4 result = *this;
 	return result += other;
 }
 
 
-Mat4& Mat4::operator*=(const Mat4& other)
+Mat4& Mat4::operator*=( const Mat4& other )
 {
 	float temp[16];
-	for (int i{ 0 }; i < 4; ++i)
+	for ( int i{ 0 }; i < 4; ++i )
 	{
-		for (int j{ 0 }; j < 4; ++j)
+		for ( int j{ 0 }; j < 4; ++j )
 		{
-			temp[i + j * 4] = matrix[i]      * other[j * 4]     +
-			                  matrix[i +  4] * other[j * 4 + 1] +
-			                  matrix[i +  8] * other[j * 4 + 2] +
-			                  matrix[i + 12] * other[j * 4 + 3];
+			temp[i + j * 4] = matrix[i] * other[j * 4] + matrix[i + 4] * other[j * 4 + 1] +
+			                  matrix[i + 8] * other[j * 4 + 2] + matrix[i + 12] * other[j * 4 + 3];
 		}
 	}
-	for (int i{ 0 }; i < 16; ++i)
+	for ( int i{ 0 }; i < 16; ++i )
 	{
 		matrix[i] = temp[i];
 	}
@@ -378,18 +375,18 @@ Mat4& Mat4::operator*=(const Mat4& other)
 }
 
 
-const Mat4 Mat4::operator*(const Mat4& other) const
+const Mat4 Mat4::operator*( const Mat4& other ) const
 {
 	Mat4 result = *this;
 	return result *= other;
 }
 
 
-const bool Mat4::operator==(const Mat4& other) const
+const bool Mat4::operator==( const Mat4& other ) const
 {
-	for (unsigned i{ 0 }; i < 16; ++i)
+	for ( unsigned i{ 0 }; i < 16; ++i )
 	{
-		if (matrix[i] != other.matrix[i])
+		if ( matrix[i] != other.matrix[i] )
 		{
 			return false;
 		}
@@ -398,7 +395,7 @@ const bool Mat4::operator==(const Mat4& other) const
 }
 
 
-void Mat4::Translate(const Vec3& vec)
+void Mat4::Translate( const Vec3& vec )
 {
 	matrix[12] += vec.x;
 	matrix[13] += vec.y;
@@ -406,110 +403,82 @@ void Mat4::Translate(const Vec3& vec)
 }
 
 
-void Mat4::TranslateX(const float amount)
+void Mat4::TranslateX( const float amount )
 {
 	matrix[12] += amount;
 }
 
 
-void Mat4::TranslateY(const float amount)
+void Mat4::TranslateY( const float amount )
 {
 	matrix[13] += amount;
 }
 
 
-void Mat4::TranslateZ(const float amount)
+void Mat4::TranslateZ( const float amount )
 {
 	matrix[14] += amount;
 }
 
 
-void Mat4::Scale(const Vec3& scale)
+void Mat4::Scale( const Vec3& scale )
 {
-	matrix[0] = scale.x;
-	matrix[5] = scale.y;
+	matrix[0]  = scale.x;
+	matrix[5]  = scale.y;
 	matrix[10] = scale.z;
 }
 
-void Mat4::ScaleX(const float scale)
+void Mat4::ScaleX( const float scale )
 {
 	matrix[0] = scale;
 }
 
 
-void Mat4::ScaleY(const float scale)
+void Mat4::ScaleY( const float scale )
 {
 	matrix[5] = scale;
 }
 
 
-void Mat4::ScaleZ(const float scale)
+void Mat4::ScaleZ( const float scale )
 {
 	matrix[10] = scale;
 }
 
 
-void Mat4::Rotate(const Quat& q)
+void Mat4::Rotate( const Quat& q )
 {
-	Mat4 q1{
-		 q.w,  q.z, -q.y, -q.x,
-		-q.z,  q.w,  q.x, -q.y,
-		 q.y, -q.x,  q.w, -q.z,
-		 q.x,  q.y,  q.z,  q.w
-	};
+	Mat4 q1{ q.w, q.z, -q.y, -q.x, -q.z, q.w, q.x, -q.y, q.y, -q.x, q.w, -q.z, q.x, q.y, q.z, q.w };
 
-	Mat4 q2{
-		 q.w,  q.z, -q.y,  q.x,
-		-q.z,  q.w,  q.x,  q.y,
-		 q.y, -q.x,  q.w,  q.z,
-		-q.x, -q.y, -q.z,  q.w
-	};
+	Mat4 q2{ q.w, q.z, -q.y, q.x, -q.z, q.w, q.x, q.y, q.y, -q.x, q.w, q.z, -q.x, -q.y, -q.z, q.w };
 
 	*this = q1 * q2 * *this;
 }
 
 
-void Mat4::RotateX(const float radians)
+void Mat4::RotateX( const float radians )
 {
-	float cosrad{ static_cast<float>(std::cos(radians)) };
-	float sinrad{ static_cast<float>(std::sin(radians)) };
-	Mat4 rotation
-	{
-		1.0f,    0.0f,   0.0f, 0.0f,
-		0.0f,  cosrad, sinrad, 0.0f,
-		0.0f, -sinrad, cosrad, 0.0f,
-		0.0f,    0.0f,   0.0f, 1.0f
-	};
+	float cosrad{ static_cast<float>( std::cos( radians ) ) };
+	float sinrad{ static_cast<float>( std::sin( radians ) ) };
+	Mat4 rotation{ 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, cosrad, sinrad, 0.0f, 0.0f, -sinrad, cosrad, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 	*this = rotation * *this;
 }
 
 
-void Mat4::RotateY(const float radians)
+void Mat4::RotateY( const float radians )
 {
-	float cosrad{ static_cast<float>(std::cos(radians)) };
-	float sinrad{ static_cast<float>(std::sin(radians)) };
-	Mat4 rotation
-	{
-		cosrad, 0.0f, -sinrad, 0.0f,
-		0.0f,   1.0f, 0.0f,    0.0f,
-		sinrad, 0.0f, cosrad,  0.0f,
-		0.0f,   0.0f, 0.0f,    1.0f
-	};
+	float cosrad{ static_cast<float>( std::cos( radians ) ) };
+	float sinrad{ static_cast<float>( std::sin( radians ) ) };
+	Mat4 rotation{ cosrad, 0.0f, -sinrad, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, sinrad, 0.0f, cosrad, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 	*this = rotation * *this;
 }
 
 
-void Mat4::RotateZ(const float radians)
+void Mat4::RotateZ( const float radians )
 {
-	float cosrad{ static_cast<float>(std::cos(radians)) };
-	float sinrad{ static_cast<float>(std::sin(radians)) };
-	Mat4 rotation
-	{
-		cosrad,  sinrad, 0.0f, 0.0f,
-		-sinrad, cosrad, 0.0f, 0.0f,
-		0.0f,    0.0f,   1.0f, 0.0f,
-		0.0f,    0.0f,   0.0f, 1.0f
-	};
+	float cosrad{ static_cast<float>( std::cos( radians ) ) };
+	float sinrad{ static_cast<float>( std::sin( radians ) ) };
+	Mat4 rotation{ cosrad, sinrad, 0.0f, 0.0f, -sinrad, cosrad, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 	*this = rotation * *this;
 }
 
@@ -518,34 +487,33 @@ Rectangle Rectangle::zero{};
 
 
 Rectangle::Rectangle()
-:	x     { 0.0f }
-,	y     { 0.0f }
-,	width { 0.0f }
-,	height{ 0.0f }
-{}
+    : x{ 0.0f }
+    , y{ 0.0f }
+    , width{ 0.0f }
+    , height{ 0.0f }
+{
+}
 
 
-const bool Rectangle::operator==(const Rectangle& other) const
+const bool Rectangle::operator==( const Rectangle& other ) const
 {
 	return x == 0.0f && y == 0.0f && width == 0.0f && height == 0.0f;
 }
 
 
-bool Rectangle::Contains(float xx, float yy)
+bool Rectangle::Contains( float xx, float yy )
 {
-	return (x <= xx && xx <= (x + width )) &&
-	       (y <= yy && yy <= (y + height));
+	return ( x <= xx && xx <= ( x + width ) ) && ( y <= yy && yy <= ( y + height ) );
 }
 
 
-bool Rectangle::Intersects(const Rectangle& other)
+bool Rectangle::Intersects( const Rectangle& other )
 {
-	return (fabs(x - other.x) * 2 < (width  + other.width )) &&
-	       (fabs(y - other.y) * 2 < (height + other.height));
+	return ( fabs( x - other.x ) * 2 < ( width + other.width ) ) && ( fabs( y - other.y ) * 2 < ( height + other.height ) );
 }
 
-bool Rectangle::Intersects(const Rectangle* other)
+bool Rectangle::Intersects( const Rectangle* other )
 {
-	return (fabs(x - other->x) * 2 < (width  + other->width )) &&
-	       (fabs(y - other->y) * 2 < (height + other->height));
+	return ( fabs( x - other->x ) * 2 < ( width + other->width ) ) &&
+	       ( fabs( y - other->y ) * 2 < ( height + other->height ) );
 }
